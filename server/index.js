79c5166418b2d46a435601ba2aa9e2051cc82edc
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2023-12-25 12:03:57 +0200

    adding files to git

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import https from 'https';
+const app = express();
+const port = 9000;
+
+
+app.use(cors());
+
+
+app.get('/', (req,res)=> { 
+    res.send("Hello World");
+});
+
+
+app.listen(port, ()=>{ 
+    console.log("Server is listening now on port " + port);
+});
